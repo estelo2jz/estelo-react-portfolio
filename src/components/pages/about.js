de90e4789e6080721d7de956306e9a661d3880cc
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-05-19 11:38:09 -0500

    update aboout with skills

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -18,10 +18,21 @@ export default function() {
               <p>with React, Python, and JavaScript.</p>
               <p>
                 I started my coding journey on an online bootcamp, I'm a fast learner 
-                and I love to code. I'm in search of a Junior Developer job, I want to gain more knowledge and skills to be a better Developer.
+                and I love to code on React. I'm in search of a Developer job, where opportunity for growth and professional development. 
+                I want to gain more knowledge and skills to be a better Developer.
                 I have attended Bottega tech <a href="https://bottega.tech">https://bottega.tech</a>, and other courses on my free time.
-                I enjoy building projects with React and Svelte, like this website for example, it's built on React.
+                I enjoy building projects with React, like this website for example, it's built on React.
               </p>
+              <h3>My skills</h3>
+              <ul>
+                  <li>GIT</li>
+                  <li>HTML/SCSS</li>
+                  <li>Back end Database</li>
+                  <li>Analytical Skills</li>
+                  <li>Responsive Design</li>
+                  <li>Libraties and frameworks</li>
+                  <li>Working with Hosting (Publishing web site)</li>
+              </ul>
               <p>
                   The Website is not Mobile-Friendly.
               </p>
