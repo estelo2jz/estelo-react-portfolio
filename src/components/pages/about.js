eeb73d52446e1bf65120c9bdb7435f3e6dc785cd
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-07-07 11:49:20 -0500

    change background color

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -3,6 +3,7 @@ import profilePicture from "../../../static/assets/images/auth/bio/headshot.jpg"
 
 export default function() {
     return (
+      <div className="wrapper">
         <div className="content-page-wrapper">
           <div className="helper-img">
             <div 
@@ -20,30 +21,31 @@ export default function() {
                 }}
             />
           </div>
-            <div className="right-column">
-              <p><strong>Estelo Abellanosa is a Full Stack Developer</strong></p> 
-              <p>with React, Python, and JavaScript.</p>
-              <p>
-                Innovative Full Stack Developer with 2 years of experience in building and maintaining websites. 
-                Proficient in HTML, SASS, JavaScript, Python, and React, plus modern libraries and frameworks. 
-                Passionate about usability and possess the knowledge and working with computer hardware.
-                I have attended Bottega tech <a href="https://bottega.tech">https://bottega.tech</a>, and <a href="https://www.udemy.com/">Udemy</a> courses on my free time.
-                I enjoy building projects with React, like this website for example, it's built on React.
-              </p>
-              <h1>My skills</h1>
-              <ul>
-                  <li>Python</li>
-                  <li>HTML/SCSS?JavaScript</li>
-                  <li>Back end Database</li>
-                  <li>Analytical Skills</li>
-                  <li>Responsive Design</li>
-                  <li>Computer Skills</li>
-                  <li>Hardware Troubleshooting</li>
-              </ul>
-              <p>
-                  The Website is not Mobile-Friendly.
-              </p>
-            </div>
+          <div className="right-column">
+            <p><strong>Estelo Abellanosa is a Full Stack Developer</strong></p> 
+            <p>with React, Python, and JavaScript.</p>
+            <p>
+              Innovative Full Stack Developer with 2 years of experience in building and maintaining websites. 
+              Proficient in HTML, SASS, JavaScript, Python, and React, plus modern libraries and frameworks. 
+              Passionate about usability and possess the knowledge and working with computer hardware.
+              I have attended Bottega tech <a href="https://bottega.tech">https://bottega.tech</a>, and <a href="https://www.udemy.com/">Udemy</a> courses on my free time.
+              I enjoy building projects with React, like this website for example, it's built on React.
+            </p>
+            <h1>My skills</h1>
+            <ul>
+                <li>Python</li>
+                <li>HTML/SCSS?JavaScript</li>
+                <li>Back end Database</li>
+                <li>Analytical Skills</li>
+                <li>Responsive Design</li>
+                <li>Computer Skills</li>
+                <li>Hardware Troubleshooting</li>
+            </ul>
+            <p>
+                The Website is not Mobile-Friendly.
+            </p>
+          </div>
         </div>
+      </div>
     )
 }
