9ffa5176e91dbf5072e4916e925b367662b13a47
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-08-15 07:54:24 -0500

    change abput photo

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,5 @@
 import React from "react";
-import profilePicture from "../../../static/assets/images/auth/bio/headshot.jpg";
+import profilePicture from "../../../static/assets/images/auth/bio/headshot2.jpg";
 
 export default function() {
     return (
