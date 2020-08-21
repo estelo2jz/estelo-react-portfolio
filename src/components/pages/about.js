790e8e6503be40820261bf0137d9ca3cc2eeae64
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-08-21 13:49:14 -0500

    Update about.js

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -25,24 +25,9 @@ export default function() {
             <p><strong>Estelo Abellanosa is a Full Stack Developer</strong></p> 
             <p>with React, Python, and JavaScript.</p>
             <p>
-              Innovative Junior Front End Developer with 2 years of experience in building and implementing websites. 
-              Proficient in HTML/SASS, JavaScript, NodeJS,  ReactJS, Python, MySQL, JSON, ExpressJS, NightmareJS, PuppeteerJS. 
+              Innovative Junior Full Stack Developer with 2 years of experience in building and implementing websites. 
+              Proficient in HTML/SASS, JavaScript, NodeJS,  ReactJS, Python, MySQL, JSON, ExpressJS, MongoDB. 
               I have never worked on a Tech Industry before, I am eager to learn new experiences and languages. 
-              I love working with NodeJs and JSON data, It's just something that catches your eyes whenever you get some data.
-            </p>
-            <h1>My skills</h1>
-            <ul>
-                <li>Computer Skills</li>
-                <li>Hardware Troubleshooting</li>
-                <li>Web Scraping</li>
-                <li>Data Structures</li>
-                <li>MongoDB</li>
-                <li>JQuery</li>
-                <li>MySQL</li>
-
-            </ul>
-            <p>
-                The Website is not Mobile-Friendly.
             </p>
           </div>
         </div>
